f8cc34fa5bd18a8444c41c85ddba83c2ee6f9504
tianle.li <tianle.li@qunar.com>
2017-04-14 19:34:57 +0800

    工程初始化

--- src/main/java/com/abecedarian/stock/Detail.java
+++ src/main/java/com/abecedarian/stock/Detail.java
@@ -48,14 +48,48 @@ public class Detail {
     }
 
     public void handleDetailInfo() throws IOException {
-        String filePath = getFileDir() + File.separator + "000625.csv";
+        String filePath = getFileDir() + File.separator + "600886.csv";
 
-        Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(new FileReader(filePath));
+//        Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(new FileReader(filePath));
+        Iterable<CSVRecord> records = CSVFormat.RFC4180.withHeader(Headers.class).parse(new FileReader(filePath));
         for (CSVRecord record : records) {
-            String one = record.get(0);
-            String two = record.get(1);
-            System.out.println(one + "\t" + two);
+            String date = record.get(Headers.date);
+            String open = record.get(Headers.open);
+            String high = record.get(Headers.high);
+            String close = record.get(Headers.close);
+            String low = record.get(Headers.low);
+            String volume = record.get(Headers.volume);
+            String price_change = record.get(Headers.price_change);
+            String p_change = record.get(Headers.p_change);
+            String ma5 = record.get(Headers.ma5);
+            String ma10 = record.get(Headers.ma10);
+            String ma20 = record.get(Headers.ma20);
+            String v_ma5 = record.get(Headers.v_ma5);
+            String v_ma10 = record.get(Headers.v_ma10);
+            String v_ma20 = record.get(Headers.v_ma20);
+            String turnover = record.get(Headers.turnover);
+
+            System.out.println(turnover);
+
         }
+    }
+
+    public enum Headers {
+        date,//日期
+        open,//开盘价
+        high,//最高价
+        close,//收盘价
+        low,//最低价
+        volume,//成交量
+        price_change,//价格变动
+        p_change,//涨跌幅
+        ma5,//5日均价
+        ma10,//10日均价
+        ma20,//20日均价
+        v_ma5,//5日均量
+        v_ma10,//10日均量
+        v_ma20,//20日均量
+        turnover,//换手率[注：指数无此项]
     }
 
 }
