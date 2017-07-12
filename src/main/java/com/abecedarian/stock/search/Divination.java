04233f8523a835e64e0ce9263dde78165dd207bc
tianle.li <tianle.li@qunar.com>
2017-07-12 20:22:38 +0800

    股票丰富

---
+++ src/main/java/com/abecedarian/stock/search/Divination.java
@@ -0,0 +1,7 @@
+package com.abecedarian.stock.search;
+
+/**
+ * Created by tianle.li on 2017/7/12.
+ */
+public class Divination {
+}
--- src/main/java/com/abecedarian/stock/search/PoolSearch.java
+++ src/main/java/com/abecedarian/stock/search/PoolSearch.java
@@ -1,5 +1,8 @@
 package com.abecedarian.stock.search;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -12,43 +15,79 @@ public class PoolSearch {
 
     public static void main(String[] args) throws IOException {
 
+        File file = new File("D:\\tushare\\result\\all\\all_history.txt");
+        if (!file.exists()) {
+            file.createNewFile();
+            file.setExecutable(true, false);
+            file.setReadable(true, false);
+            file.setWritable(true, false);
+        }
+        FileWriter fw = new FileWriter(file.getAbsoluteFile());
+        BufferedWriter bw = new BufferedWriter(fw);
+
+
         List<String> stocks = traverseFolder1("D:\\tushare\\result\\stock");
 
         for (String stock : stocks) {
-            handleStock(stock);
+            handleStock(stock, bw);
         }
+        bw.close();
         System.out.println("dd");
     }
 
-    private static void handleStock(String filePath) {
+    private static void handleStock(String filePath, BufferedWriter bw) {
         try {
+
+
             BufferedReader br = new BufferedReader(new InputStreamReader((new FileInputStream(filePath)), "utf-8"));
             String line = null;
             while ((line = br.readLine()) != null) {
-                String[] items = line.split(",");
-                if (items.length < 10 || items[0].equals("date")) {
-                    continue;
+                try {
+                    String[] items = line.split(",");
+                    if (items.length < 10 || items[0].equals("date")) {
+                        continue;
+                    }
+                    String code = filePath.split("\\\\")[4].substring(0, 6);
+                    String date = items[0];
+                    String open = items[1];
+                    String high = items[2];
+                    String close = items[3];
+                    String low = items[4];
+                    String volume = items[5];
+                    String price_change = items[6];
+                    String p_change = items[7];
+                    String ma5 = items[8];
+                    String ma10 = items[9];
+                    String ma20 = items[10];
+                    String v_ma5 = items[11];
+                    String v_ma10 = items[12];
+                    String v_ma20 = items[13];
+                    String turnover = items[14];
+
+                    JsonObject obj = new JsonObject();
+                    obj.add("code", new JsonPrimitive(code));
+                    obj.add("date", new JsonPrimitive(date));
+                    obj.add("open", new JsonPrimitive(open));
+                    obj.add("high", new JsonPrimitive(high));
+                    obj.add("close", new JsonPrimitive(close));
+                    obj.add("low", new JsonPrimitive(low));
+                    obj.add("volume", new JsonPrimitive(volume));
+                    obj.add("price_change", new JsonPrimitive(price_change));
+                    obj.add("p_change", new JsonPrimitive(p_change));
+                    obj.add("ma5", new JsonPrimitive(ma5));
+                    obj.add("ma10", new JsonPrimitive(ma10));
+                    obj.add("ma20", new JsonPrimitive(ma20));
+                    obj.add("v_ma5", new JsonPrimitive(v_ma5));
+                    obj.add("v_ma10", new JsonPrimitive(v_ma10));
+                    obj.add("v_ma20", new JsonPrimitive(v_ma20));
+                    obj.add("turnover", new JsonPrimitive(turnover));
+
+                    bw.write(obj.toString());
+                    bw.newLine();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                String date=items[0];
-                String open=items[1];
-                String high=items[2];
-                String close=items[3];
-                String low=items[4];
-                String volume=items[5];
-                String price_change=items[6];
-                String p_change=items[7];
-                String ma5=items[8];
-                String ma10=items[9];
-                String ma20=items[10];
-                String v_ma5=items[11];
-                String v_ma10=items[12];
-                String v_ma20=items[13];
-                String turnover=items[14];
-                System.out.println(line);
-
-                System.out.println("dd");
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
